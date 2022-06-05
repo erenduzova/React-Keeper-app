d6e99a352cdd4b4814c4a0f568848b00ee3555ec
Eren Düzova <erenduzova@gmail.com>
2022-06-05 23:44:17 +0300

    Basic React App codes added to index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(
+  <React.StrictMode>
+    <h1>Test Heading</h1>
+  </React.StrictMode>
+);
