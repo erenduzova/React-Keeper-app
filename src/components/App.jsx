2dfc1df16291dbfc99c1cf2eb7096e63cf33a0e3
Eren Düzova <erenduzova@gmail.com>
2022-07-01 21:54:51 +0300

    NewNoteArea.jsx created for new notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,16 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import NewNoteArea from "./NewNoteArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map( note => <Note key={note.key} title={note.title} content={note.content} />)}
+      <NewNoteArea />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
